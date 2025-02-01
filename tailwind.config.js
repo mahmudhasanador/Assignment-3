28ea5c6c95e0a6e0defe9fd0bab871cfdf386dd8
Mahmud Hasan Ador <mahmudhasanador@gmail.com>
2025-02-01 19:21:27 +0600

    completed the popular productrs part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      extend: {
-        animation: {
-          scaleLoop: "scaleLoop 2s infinite ease-in-out",
-        },
-        keyframes: {
-          scaleLoop: {
-            "0%, 100%": { transform: "scale(1)" },
-            "50%": { transform: "scale(1.1)" },
-          },
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora', 'serif'], // Custom font family
+      },
+      keyframes: {
+        scaleLoop: {
+          '0%, 100%': { transform: 'scale(0.9)' },
+          '50%': { transform: 'scale(0.99)' },
         },
       },
+      animation: {
+        scaleLoop: 'scaleLoop 2s infinite ease-in-out', // Register animation
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
